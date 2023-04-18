134b3d6fb961e4979862a2754721b0239becb855
darkoboy <118077486+dewaahr@users.noreply.github.com>
2023-04-18 19:00:47 +0700

    Update Scode_1.py

--- Scode_1.py
+++ Scode_1.py
@@ -3,6 +3,7 @@ text_b=open('soal_1b.txt','r')
 baris_texta = text_a.readlines()
 baris_textb = text_b.readlines()
 
+#cacad
 for baris_a in baris_texta:
     for baris_b in baris_textb:
         if baris_a == baris_b:
