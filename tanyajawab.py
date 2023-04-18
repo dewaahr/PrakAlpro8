988a7827892d0ea1be1221016b36be053da5aeac
darkoboy <118077486+dewaahr@users.noreply.github.com>
2023-04-18 17:51:39 +0700

    Add files via upload

--- tanyajawab.py
+++ tanyajawab.py
@@ -1,51 +1,26 @@
-# nama_file='data.txt'
-# handle=open('data.txt','r')
-# isi_file=handle.read() #membaca keseluruhan file
-# handle.close() #menutup file
-
-# "\\ = pembatas directory"
-
-
-
-# a=open('soal.txt','r')
-# baris=1
-# for kata in a:
-#     if kata.startswith('C'):
-#         print(f'{baris}.{kata}',end="")
-#         baris=baris+1
-# a.close()
-
-
-
-print('Daftar matkul')
-a=open('matkul.txt','r')
-baris=1
-totalsks=0
-totalbobot=0
-sks=0
-matkul=''
-harus_diulang=''
-for kata in a:
-    if baris%3==1:
-        print(kata,end="")
-        matakuliah=kata.strip()
-    elif baris%3==2:
-        totalsks=totalsks+int(kata)
-        sks=int(kata)
-    elif baris%3==0:
-
-        if kata.strip()=='A':
-            totalbobot=totalbobot+sks*4
-        elif kata.strip()=='B':
-            totalbobot=totalbobot+sks*3
-        elif kata.strip()=='C':
-            totalbobot=totalbobot+sks*3
-        elif kata.strip()=='D':
-            totalbobot=totalbobot+sks*1
-        elif kata.strip()=='E':
-            harus_diulang=harus_diulang+matakuliah+"\n"
-    baris=baris+1
-print(totalbobot/totalsks)
-print(f'Total SKS :{totalsks:.2f}')
-print(f'Harus diulang:\n{harus_diulang}')
-a.close()
+'''file=input('Nama File:')
+with open(file,'r') as file:
+    for kalimat in file:
+        kalimat=kalimat.lower()
+        bagian=kalimat.split('||')
+        kata1=bagian[0].strip()
+        kata2=bagian[1].strip()
+        print (kata1)
+        print(kata2)
+        jawab=input('Jawab:').lower()
+        if jawab==kata2:
+            print('Jawaban Benar!')
+        else: print('Jawaban Salah')'''
+file=input('Nama File:')
+a=open(file,'r')
+for kalimat in a:
+    kalimat=kalimat.lower()
+    bagian=kalimat.split('||')
+    kata1=bagian[0].strip()
+    kata2=bagian[1].strip()
+    print (kata1)
+    jawab=input('Jawab:').lower()
+    if jawab==kata2:
+        print('Jawaban Benar!')
+    else: print('Jawaban Salah')
+        
